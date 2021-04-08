1afb52fd9d7e33a58f872b3dcf1817b8e6198056
dnieln7 <dnieln7@gmail.com>
2021-04-08 18:15:00 -0500

    removed previous changes

--- api/config/config.js
+++ api/config/config.js
@@ -11,13 +11,7 @@ module.exports = {
         database: process.env.DB_DATABASE,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        dialect: 'postgresql',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
+        dialect: 'postgresql'
     },
     test: {
         username: process.env.DB_USERNAME,
